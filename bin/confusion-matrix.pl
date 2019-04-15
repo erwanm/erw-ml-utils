183658f2a24dc56920129542ac742078f16a7519
Erwan Moreau <moreaue@scss.tcd.ie>
2019-04-15 21:00:17 +0100

    added options to confusion-matrix

--- bin/confusion-matrix.pl
+++ bin/confusion-matrix.pl
@@ -7,6 +7,8 @@ use Getopt::Std;
 my %res;
 my %keys2;
 my $total=0;
+my $allKeys=0;
+my $onlyAccu=0;
 
 sub usage {
     my $fh = shift;
@@ -16,23 +18,31 @@ sub usage {
     print $fh "  Reads a 3 columns input <key1> <key2> <val> from STDIN and writes a\n";
     print $fh "  two ways table to STDOUT.\n";
     print $fh "   -d: also prints details (percentage, precision/recall/f-measures by line)\n";
+    print $fh "   -p: print all the possible keys as line and column (square matrix).\n";
+    print $fh "   -a: only output accuracy\n";
     print $fh "\n";
 }
 
 # PARSING OPTIONS                                                                                                                                                       
 my %opt;
-getopts('hd', \%opt ) or  ( print STDERR "Error in options" &&  usage(*STDERR) && exit 1);
+getopts('hadp', \%opt ) or  ( print STDERR "Error in options" &&  usage(*STDERR) && exit 1);
 usage($STDOUT) && exit 0 if $opt{h};
 print STDERR "0 arguments expected but ".scalar(@ARGV)." found: ".join(" ; ", @ARGV)  && usage(*STDERR) && exit 1 if (scalar(@ARGV) != 0);
 my $details=defined($opt{d});
+$allKeys = defined($opt{p});
+$onlyAccu = defined($opt{a});
+
+my $countCorrect = 0;
 
 my %positiveVal2;
 while (<STDIN>) {
     chomp;
     my @cols = split;
     if (scalar(@cols) == 3) {
-	$res{$cols[0]}->{$cols[1]} = $cols[2];
+	$res{$cols[0]}->{$cols[1]} += $cols[2]; # fix: acculumating intead of assigning in order to take into account multiple lines with same pair of keys
+#	print STDERR "DEBUG1 res{$cols[0]}->{$cols[1]}: +$cols[2] = ".$res{$cols[0]}->{$cols[1]}."\n";
 	$keys2{$cols[1]} = 1;
+	$countCorrect += $cols[2] if ($cols[0] eq $cols[1]);
 	$total += $cols[2];
 	$positiveVal2{$cols[1]} +=$cols[2];
     } else {
@@ -40,33 +50,53 @@ while (<STDIN>) {
 	exit 1;
     }
 }
-print "\t\t".join("\t\t", sort keys %keys2);
-print "\t\tPrec.%\t\tRecall%\t\tF1%" if ($details);
-print "\n";
-foreach my $val1 (sort keys %res) {
-    print "$val1";
-    my $n;
-    my $actualVal1 = 0;
-    foreach my $val2 (keys %keys2) {
-	$n = $res{$val1}->{$val2};
-	$n = 0 if (!defined($n));
-	$actualVal1+=$n;
-#	if ($details) {
-#	    printf("\t\t$n [%.2f%%]", $n/$total*100);
-#	} else {
-	    printf("\t\t$n");
-#	}
+
+if ($onlyAccu) {
+    my $accu = sprintf("\t%.2f", $countCorrect / $total * 100);
+    print "$countCorrect\t$total\t$accu\n";
+} else {
+
+    if ($allKeys) {
+	foreach my $k1 (keys %res) {
+	    $keys2{$k1} = 1;
+	}
+	foreach my $k2 (keys %keys2) {
+	    foreach my $k1 (keys %res) {
+		$res{$k2}->{$k1} = 0 if (!defined($res{$k2}->{$k1}));
+	    }
+	}
     }
-    if ($details) {
-	my $tp = $res{$val1}->{$val1};
-	$tp=0 if (!defined($tp));
-	my $prec = (defined($positiveVal2{$val1}))?($tp / $positiveVal2{$val1}):"NaN";
-	my $recall = ($actualVal1>0)?($tp / $actualVal1):"NaN";
-	my $f1 = (($prec ne "NaN")&&($recall ne "NaN") && ($prec+$recall>0) )?( 2 * $prec * $recall / ($prec + $recall)):"NaN";
-	my $str = ($prec ne "NaN")?sprintf("\t\t%.2f", $prec*100):"\t\tNaN";
-	$str .= ($recall ne "NaN")?sprintf("\t\t%.2f", $recall*100):"\t\tNaN";
-	$str .= ($f1 ne "NaN")?sprintf("\t\t%.2f", $f1*100):"\t\tNaN";
-	print "$str";
-    }
+
+    print "\t".join("\t", sort keys %keys2);
+    print "\tPrec.%\tRecall%\tF1%" if ($details);
     print "\n";
+    foreach my $val1 (sort keys %res) {
+	print "$val1";
+	my $n;
+	my $actualVal1 = 0;
+	foreach my $val2 (sort keys %keys2) {
+	    $n = $res{$val1}->{$val2};
+	    $n = 0 if (!defined($n));
+	    $actualVal1+=$n;
+	    #	if ($details) {
+	    #	    printf("\t$n [%.2f%%]", $n/$total*100);
+	    #	} else {
+	    #	print STDERR "DEBUG2 res{$val1}->{$val2}= $n\n";
+	    
+	    printf("\t$n");
+	    #	}
+	}
+	if ($details) {
+	    my $tp = $res{$val1}->{$val1};
+	    $tp=0 if (!defined($tp));
+	    my $prec = (defined($positiveVal2{$val1}))?($tp / $positiveVal2{$val1}):"NaN";
+	    my $recall = ($actualVal1>0)?($tp / $actualVal1):"NaN";
+	    my $f1 = (($prec ne "NaN")&&($recall ne "NaN") && ($prec+$recall>0) )?( 2 * $prec * $recall / ($prec + $recall)):"NaN";
+	    my $str = ($prec ne "NaN")?sprintf("\t%.2f", $prec*100):"\tNaN";
+	    $str .= ($recall ne "NaN")?sprintf("\t%.2f", $recall*100):"\tNaN";
+	    $str .= ($f1 ne "NaN")?sprintf("\t%.2f", $f1*100):"\tNaN";
+	    print "$str";
+	}
+	print "\n";
+    }
 }
